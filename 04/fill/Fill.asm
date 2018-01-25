780f1e95a93635e2d7e2aaa798d7f4ecfe365c52
freemanz625 <zfreeman@smu.edu>
2018-01-25 01:37:09 -0600

    Fill.hack

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @SCREEN
+    D = A
+    @off
+    M = D
+
+(LOOP)
+    @KBD
+    D = M
+    @CLEAR
+    D;JEQ
+    @off
+    A = M
+    M = -1
+    A = A + 1
+    M = -1
+    D = A + 1
+    @off
+    M = D
+    @LOOP
+    0;JMP
+(CLEAR)
+    @KBD
+    D = M
+    @LOOP
+    D;JNE
+    @off
+    D = M
+    @SCREEN
+    D = D - A
+    @LOOP
+    D;JEQ
+    @off
+    A = M
+    M = 0
+    A = A - 1
+    M = 0
+    D = A - 1
+    A = D
+    M = 0
+    @off
+    M = D
+    @CLEAR
+    0;JMP
+
+ 
